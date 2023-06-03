2dec7c3517c617c2764a5b10c3eab04c710a8cc7
Semetery29 <mdazizulhakimsuny@gmail.com>
2023-06-03 07:14:17 +0600

    final project day-2, until tabs

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,15 +1,39 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../../assets/menu/salad-bg.jpg'
+import soupImg from '../../../assets/menu/soup-bg.jpg'
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <Cover img={menuImg} title='Our Menu'/>
-            
+            <Cover img={menuImg} title='Our Menu' />
+            {/* main cover */}
+            <SectionTitle heading="Today's Offer" subHeading="Don't Miss" />
+            {/* offered menu items */}
+            <MenuCategory items={offered} />
+            {/* desert menu items */}
+            <MenuCategory items={desserts} title="Dessert" img={dessertImg} />
+            {/* pizza menu items */}
+            <MenuCategory items={pizza} title="Pizza" img={pizzaImg} />
+            {/* salad menu items */}
+            <MenuCategory items={salad} title="salad" img={saladImg} />
+            {/* soup menu items */}
+            <MenuCategory items={soup} title="Soups" img={soupImg} />
         </div>
     );
 };
---
+++ src/pages/Order/Order/Order.jsx
@@ -0,0 +1,56 @@
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import orderCover from '../../../assets/shop/banner2.jpg'
+import Cover from '../../Shared/Cover/Cover';
+import 'react-tabs/style/react-tabs.css';
+import { useState } from 'react';
+import useMenu from '../../../hooks/useMenu';
+import OrderTab from '../OrderTab/OrderTab';
+import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+
+const Order = () => {
+    const categories = ['salad', 'pizza', 'soup', 'dessert', 'drinks'];
+    const { category } = useParams();
+    const initialIndex = categories.indexOf(category)
+    const [tabIndex, setTabIndex] = useState(initialIndex);
+    const [menu] = useMenu();
+
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const drinks = menu.filter(item => item.category === 'drinks');
+
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boss | Order Food</title>
+            </Helmet>
+            <Cover img={orderCover} title="order food" />
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)} className={"text-center"}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soups</Tab>
+                    <Tab>Desserts</Tab>
+                    <Tab>Drinks(HARAM! Bro)</Tab>
+                </TabList>
+                <TabPanel>
+                    <OrderTab items={salad} />
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab items={pizza} />
+                </TabPanel>
+                <OrderTab items={soup} />
+                <TabPanel>
+                    <OrderTab items={desserts} />
+                </TabPanel>
+                <TabPanel>
+                    <OrderTab items={drinks} />
+                </TabPanel>
+            </Tabs>
+        </div>
+    );
+};
+
+export default Order;
